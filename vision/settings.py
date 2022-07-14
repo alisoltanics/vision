c289826b3528ef2a52f81e7d840b8225c82bb1d3
Asliasghar Soltani <alisoltanics@gmail.com>
2022-07-14 12:01:10 +0430

    add yml files

--- vision/settings.py
+++ vision/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1',
+  '192.168.49.2'
+]
 
 # Application definition
 
@@ -77,11 +81,11 @@ WSGI_APPLICATION = 'vision.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_NAME'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'), 
-        'HOST': config('POSTGRES_SERVICE_HOST'),
-        'PORT': config('POSTGRES_SERVICE_PORT')
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'), 
+        'HOST': os.environ.get('POSTGRES_SERVICE_HOST'),
+        'PORT': os.environ.get('POSTGRES_SERVICE_PORT')
     }
 }
 
